5a62ed7f73cf7b2ece9fbb45da26bb43730a8ff2
andrewparr <10243811+andrewparr@users.noreply.github.com>
2021-09-01 18:38:55 +0100

    Pygame Platformer Part 15: Powerups

--- src/platform/sprites.py
+++ src/platform/sprites.py
@@ -1,7 +1,7 @@
 # Sprite classes for platform game
 import pygame as pg
 from settings import *
-from random import choice
+from random import choice, randrange
 vec = pg.math.Vector2
 
 class SpriteSheet:
@@ -18,7 +18,8 @@ class SpriteSheet:
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
-        pg.sprite.Sprite.__init__(self)
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
         self.walking = False
         self.jumping = False
@@ -118,10 +119,10 @@ class Player(pg.sprite.Sprite):
                 self.rect = self.image.get_rect()
                 self.rect.bottom = bottom
 
-
 class Platform(pg.sprite.Sprite):
-    def __init__(self, game, x, y) -> None:
-        super().__init__()
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.platforms
+        pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
         images = [
             self.game.spritesheet.get_image(0, 288, 380, 94),
@@ -132,3 +133,23 @@ class Platform(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        if randrange(100) < POW_SPAWN_PCT:
+            Pow(self.game, self)
+
+class Pow(pg.sprite.Sprite):
+    def __init__(self, game, plat):
+        self.groups = game.all_sprites, game.powerups
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.plat = plat
+        self.type = choice(['boost'])
+        self.image = self.game.spritesheet.get_image(820, 1805, 71, 70)
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.plat.rect.centerx
+        self.rect.bottom = self.plat.rect.top - 5
+
+    def update(self):
+        self.rect.bottom = self.plat.rect.top - 5
+        if not self.game.platforms.has(self.plat):
+            self.kill()
