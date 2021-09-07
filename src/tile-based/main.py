6448f6c9ec24d551d25f9602deb6a2d333d3ac84
andrewparr <10243811+andrewparr@users.noreply.github.com>
2021-09-07 18:15:36 +0100

    Tile-based game Part 2: Collisions and Tilemap

--- src/tile-based/main.py
+++ src/tile-based/main.py
@@ -3,6 +3,7 @@
 
 import pygame as pg
 import sys
+from os import path
 from tsettings import *
 from tsprites import *
 
@@ -16,15 +17,22 @@ class Game:
         self.load_data()
 
     def load_data(self):
-        pass
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
 
     def new(self):
         # initialise all variables and do all setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        self.player = Player(self, 10, 10)
-        for x in range(10, 20):
-            Wall(self, x, 5)
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
 
     def run(self):
         # Game Loop - set self.playing = False to end the game
