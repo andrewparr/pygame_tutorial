12547b96457a70653182d5154e2f0f330e0c572b
andrewparr <10243811+andrewparr@users.noreply.github.com>
2021-09-11 21:02:42 +0100

    Tile-based game Part 4: Scrolling Map / Camera

--- src/tile-based/main.py
+++ src/tile-based/main.py
@@ -6,6 +6,7 @@ import sys
 from os import path
 from tsettings import *
 from tsprites import *
+from tilemap import *
 
 class Game:
     def __init__(self):
@@ -18,21 +19,19 @@ class Game:
 
     def load_data(self):
         game_folder = path.dirname(__file__)
-        self.map_data = []
-        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
-            for line in f:
-                self.map_data.append(line)
+        self.map = Map(path.join(game_folder, 'map2.txt'))
 
     def new(self):
         # initialise all variables and do all setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        for row, tiles in enumerate(self.map_data):
+        for row, tiles in enumerate(self.map.data):
             for col, tile in enumerate(tiles):
                 if tile == '1':
                     Wall(self, col, row)
                 if tile == 'P':
                     self.player = Player(self, col, row)
+        self.camera = Camera(self.map.width, self.map.height)
 
     def run(self):
         # Game Loop - set self.playing = False to end the game
@@ -50,6 +49,7 @@ class Game:
     def update(self):
         # update portion of game loop
         self.all_sprites.update()
+        self.camera.update(self.player)
 
     def draw_grid(self):
         for x in range(0, WIDTH, TILESIZE):
@@ -60,7 +60,8 @@ class Game:
     def draw(self):
         self.screen.fill(BGCOLOR)
         self.draw_grid()
-        self.all_sprites.draw(self.screen)
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
         pg.display.flip()
 
     def events(self):
