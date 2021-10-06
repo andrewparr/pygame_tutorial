b798a84c0fcc5e25f5ff32c3068dedda4caa3dff
andrewparr <10243811+andrewparr@users.noreply.github.com>
2021-10-06 20:29:10 +0100

    Tile-based game Part 15: Simple Visual Effects (and a bug fix!)

--- src/tile-based/tsettings.py
+++ src/tile-based/tsettings.py
@@ -42,7 +42,6 @@ KICKBACK = 200
 GUN_SPREAD = 5
 BULLET_DAMAGE = 10
 
-
 # Mob settings
 MOB_IMG = 'zombie1_hold.png'
 MOB_SPEEDS = [150, 100, 75, 125]
@@ -51,3 +50,15 @@ MOB_HEALTH = 100
 MOB_DAMAGE = 10
 MOB_KNOCKBACK = 20
 AVOID_RADIUS = 50
+
+# Effects
+MUZZLE_FLASHES = ['whitePuff15.png', 'whitePuff16.png', 'whitePuff17.png',
+                  'whitePuff18.png']
+FLASH_DURATION = 40
+
+# Layers
+WALL_LAYER = 1
+PLAYER_LAYER = 2
+BULLET_LAYER = 3
+MOB_LAYER = 2
+EFFECTS_LAYER = 4
